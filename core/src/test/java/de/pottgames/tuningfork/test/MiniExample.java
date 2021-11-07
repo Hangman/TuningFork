5e88e24c620749423a1827d5d4e702e084362cd0
Hangman <webmaster@pottgames.de>
2021-11-07 02:39:32 +0100

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/Hangman/TuningFork.git
    
    Signed-off-by: Hangman <webmaster@pottgames.de>

--- core/src/test/java/de/pottgames/tuningfork/test/MiniExample.java
+++ core/src/test/java/de/pottgames/tuningfork/test/MiniExample.java
@@ -1,20 +1,44 @@
 package de.pottgames.tuningfork.test;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+import de.pottgames.tuningfork.Audio;
+import de.pottgames.tuningfork.SoundBuffer;
+import de.pottgames.tuningfork.WaveLoader;
+
 public class MiniExample extends ApplicationAdapter {
+    private Audio       audio;
+    private SoundBuffer sound;
+
 
     @Override
     public void create() {
+        // before we can do anything, we need to initialize our Audio instance
+        this.audio = Audio.init();
 
+        // load a sound
+        this.sound = WaveLoader.load(Gdx.files.internal("src/test/resources/numbers.wav"));
+
+        // play the sound
+        this.audio.play(this.sound);
     }
 
 
     @Override
     public void render() {
+        // we chill in a black window
+    }
 
+
+    @Override
+    public void dispose() {
+        this.sound.dispose();
+
+        // always dispose Audio last
+        this.audio.dispose();
     }
 
 
