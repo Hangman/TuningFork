e711e7b1b81a446d505d8b197479aaac8e207807
Matthias <webmaster@pottgames.de>
2022-01-07 00:25:29 +0100

    Add StreamedSoundSourceTest
    Signed-off-by: Matthias <webmaster@pottgames.de>

---
+++ core/src/test/java/de/pottgames/tuningfork/test/StreamedSoundSourceTest.java
@@ -0,0 +1,48 @@
+package de.pottgames.tuningfork.test;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+
+import de.pottgames.tuningfork.Audio;
+import de.pottgames.tuningfork.StreamedSoundSource;
+
+public class StreamedSoundSourceTest extends ApplicationAdapter {
+    private Audio               audio;
+    private StreamedSoundSource source;
+
+
+    @Override
+    public void create() {
+        this.audio = Audio.init();
+
+        this.source = new StreamedSoundSource(Gdx.files.internal("src/test/resources/numbers2.ogg"));
+        this.source.setLooping(true);
+        this.source.play();
+    }
+
+
+    @Override
+    public void render() {
+        // we chill in a black window
+    }
+
+
+    @Override
+    public void dispose() {
+        this.source.dispose();
+        this.audio.dispose();
+    }
+
+
+    public static void main(String[] args) {
+        final Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setTitle("StreamedSoundSourceTest");
+        config.setWindowedMode(1000, 800);
+        config.useVsync(true);
+        config.disableAudio(true);
+        new Lwjgl3Application(new StreamedSoundSourceTest(), config);
+    }
+
+}
