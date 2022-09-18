32aebf98125bae86408781228fef928aa044d6ff
Matthias <webmaster@pottgames.de>
2022-09-18 04:18:48 +0200

    Fix Mp3LoadDemo window title
    Signed-off-by: Matthias <webmaster@pottgames.de>

--- core/src/test/java/de/pottgames/tuningfork/test/Mp3LoadDemo.java
+++ core/src/test/java/de/pottgames/tuningfork/test/Mp3LoadDemo.java
@@ -91,7 +91,7 @@ public class Mp3LoadDemo extends ApplicationAdapter {
 
     public static void main(String[] args) {
         final Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        config.setTitle("Mp3LoadTest");
+        config.setTitle("Mp3LoadDemo");
         config.setWindowedMode(1000, 800);
         config.useVsync(true);
         config.disableAudio(true);
