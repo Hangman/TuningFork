1a1d749a13f3c4eca5f60bec42a453afc3298156
Matthias <webmaster@pottgames.de>
2023-08-23 12:43:45 +0200

    Update PlayReverseExample
    Signed-off-by: Matthias <webmaster@pottgames.de>

--- core/src/test/java/de/pottgames/tuningfork/test/PlayReverseExample.java
+++ core/src/test/java/de/pottgames/tuningfork/test/PlayReverseExample.java
@@ -24,8 +24,8 @@ import de.pottgames.tuningfork.Audio;
 import de.pottgames.tuningfork.SoundBuffer;
 import de.pottgames.tuningfork.SoundBufferLoader;
 import de.pottgames.tuningfork.SoundBufferLoader.SoundBufferLoaderParameter;
+import de.pottgames.tuningfork.SoundLoader;
 import de.pottgames.tuningfork.SoundSource;
-import de.pottgames.tuningfork.WaveLoader;
 
 public class PlayReverseExample extends ApplicationAdapter {
     private boolean      jobDone = false;
@@ -39,10 +39,16 @@ public class PlayReverseExample extends ApplicationAdapter {
     @Override
     public void create() {
         this.audio = Audio.init();
-
         this.setupAssetManager();
-        this.load();
-        this.loadAsync();
+
+        // normal loading
+        this.sound = SoundLoader.loadReverse(Gdx.files.internal("numbers.wav"));
+
+        // async loading via AssetManager
+        final SoundBufferLoaderParameter parameter = new SoundBufferLoaderParameter();
+        parameter.reverse = true;
+        this.assetManager.load("carnivalrides.ogg", SoundBuffer.class, parameter);
+
         this.source = this.audio.obtainSource(this.sound);
         this.source.play();
     }
@@ -52,24 +58,6 @@ public class PlayReverseExample extends ApplicationAdapter {
         this.assetManager = new AssetManager();
         final FileHandleResolver resolver = new InternalFileHandleResolver();
         this.assetManager.setLoader(SoundBuffer.class, new SoundBufferLoader(resolver));
-    }
-
-
-    public void load() {
-        // use any of these
-
-        // this.sound = AiffLoader.loadReverse(Gdx.files.internal("numbers.aiff"));
-        // this.sound = FlacLoader.loadReverse(Gdx.files.internal("numbers_16bit_stereo.flac"));
-        // this.sound = Mp3Loader.loadReverse(Gdx.files.internal("numbers.mp3"));
-        // this.sound = OggLoader.loadReverse(Gdx.files.internal("numbers2.ogg"));
-        this.sound = WaveLoader.loadReverse(Gdx.files.internal("numbers.wav"));
-    }
-
-
-    private void loadAsync() {
-        final SoundBufferLoaderParameter parameter = new SoundBufferLoaderParameter();
-        parameter.reverse = true;
-        this.assetManager.load("carnivalrides.ogg", SoundBuffer.class, parameter);
     }
 
 
